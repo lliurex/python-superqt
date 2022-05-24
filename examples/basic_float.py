0e9e4b56118efed7c2ec6c26c17e13601f14c52e
M.Angel Juan <m.angel.juan@gmail.com>
2022-05-24 09:32:14 +0200

    Initial code from 0.3.2

---
+++ examples/basic_float.py
@@ -0,0 +1,13 @@
+from qtpy.QtCore import Qt
+from qtpy.QtWidgets import QApplication
+
+from superqt import QDoubleSlider
+
+app = QApplication([])
+
+slider = QDoubleSlider(Qt.Orientation.Horizontal)
+slider.setRange(0, 1)
+slider.setValue(0.5)
+slider.show()
+
+app.exec_()
